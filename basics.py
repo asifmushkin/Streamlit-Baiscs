04ec4e6050572bb3e02cd43c728c5290b8347583
Asif khan <asifmushkin22@gmail.com>
2024-01-15 11:16:50 +0400

    updated files

--- basics.py
+++ basics.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.text("myfile")
+st.latex(r''' e^3 = 2x-6''') # for researcher its changes style as per publisher
+
+#Display media
+st.image("D:\_4.Docs\Dcouments\passport.jpeg")
